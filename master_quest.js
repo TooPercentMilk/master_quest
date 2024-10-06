4b5751b12434968341c12599f8d4e5dcea29655d
Bradley Kerr <73859763+TooPercentMilk@users.noreply.github.com>
2024-10-06 16:39:31 -0400

    Skull and Egg toggle functionality
    Replaced the checkboxes with skull and egg icons respectively. Made js code that dims or illuminates the icons based on their completion.

--- master_quest.js
+++ master_quest.js
@@ -1,2 +1,37 @@
-let bone_list = new Array(22).fill(false);
+let bone_list = new Array(21).fill(false);
 let egg_list = new Array(8).fill(false);
+
+const skull_images = document.querySelectorAll('input[type="image"].skull');
+const egg_images = document.querySelectorAll('input[type="image"].egg');
+
+function updateImageShading(image, status) {
+    if (status) {
+      image.classList.remove('darkened'); // Remove darkening when true
+    } else {
+      image.classList.add('darkened'); // Apply darkening when false
+    }
+  }
+
+// Loop through each image
+skull_images.forEach((image, index) => {
+    updateImageShading(image, bone_list[index]);
+    // Add a click event listener to each image
+    image.addEventListener('click', function () {
+    // Toggle the boolean value in the imageStatusList for the corresponding index
+    bone_list[index] = !bone_list[index];
+    console.log(`Image ${index + 1} clicked. Status: ${bone_list[index]}`);
+    updateImageShading(image, bone_list[index]);
+  });
+});
+
+egg_images.forEach((image, index) => {
+    updateImageShading(image, egg_list[index]);
+    // Add a click event listener to each image
+    image.addEventListener('click', function () {
+      // Toggle the boolean value in the imageStatusList for the corresponding index
+      egg_list[index] = !egg_list[index];
+      console.log(`Image ${index + 1} clicked. Status: ${egg_list[index]}`);
+      updateImageShading(image, egg_list[index]);
+    });
+  });
+
