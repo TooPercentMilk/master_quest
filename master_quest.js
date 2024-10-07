83b1f3031ffdf4ce6fefa69cdbef141683fc56cf
Bradley Kerr <73859763+TooPercentMilk@users.noreply.github.com>
2024-10-07 10:22:26 -0400

    Stats Panel Functionality
    Added counters that increment every time a skull or egg are toggled

--- master_quest.js
+++ master_quest.js
@@ -1,12 +1,22 @@
 let bone_list = new Array(21).fill(false);
 let egg_list = new Array(8).fill(false);
+let bone_count = 0;
+let egg_count = 0;
+
+let bone_count_element = document.getElementById("bone_count_display");
+let egg_count_element = document.getElementById("egg_count_display");
 
 const skull_images = document.querySelectorAll('input[type="image"].skull');
 const egg_images = document.querySelectorAll('input[type="image"].egg');
 
 function updateImageShading(image, status) {
+    bone_count = bone_list.filter(value => value === true).length;
+    bone_count_element.innerHTML = bone_count;
+    egg_count = egg_list.filter(value => value === true).length;
+    egg_count_element.innerHTML = egg_count;
     if (status) {
       image.classList.remove('darkened'); // Remove darkening when true
+      bone_count = trueCount;
     } else {
       image.classList.add('darkened'); // Apply darkening when false
     }
